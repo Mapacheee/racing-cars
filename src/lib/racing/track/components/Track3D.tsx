07042baa57b29d8b465d95d2e29d175865ddf020
Benjamín Isasmendi C. <bisasmendic@gmail.com>
2025-08-15 18:26:27 -0400

    removed 3D models for Track, now using mesh

--- src/lib/racing/track/components/Track3D.tsx
+++ src/lib/racing/track/components/Track3D.tsx
@@ -1,5 +1,3 @@
-import { useGLTF } from '@react-three/drei'
-import { RigidBody, interactionGroups } from '@react-three/rapier'
 import type { JSX } from 'react'
 import type { TrackPiece } from '../types/index'
 import { ROAD_GEOMETRY } from '../systems/TrackSystem'
@@ -18,38 +16,17 @@ function TrackPieceComponent({
     piece: TrackPiece
     enablePhysics?: boolean
 }): JSX.Element {
-    if (piece.model === 'road_segment') {
-        return (
-            <mesh position={piece.position} rotation={piece.rotation}>
-                <boxGeometry
-                    args={[
-                        ROAD_GEOMETRY.width,
-                        ROAD_GEOMETRY.height,
-                        ROAD_GEOMETRY.length,
-                    ]}
-                />
-                <meshStandardMaterial color="#444444" />
-            </mesh>
-        )
-    }
-
-    // for custom 3d models (future expansion)
-    const { scene } = useGLTF(`/assets/models/${piece.model}`)
-    const modelPrimitive = (
-        <primitive
-            object={scene.clone()}
-            position={piece.position}
-            rotation={piece.rotation}
-            scale={1}
-        />
-    )
-
-    return enablePhysics ? (
-        <RigidBody type="fixed" colliders="trimesh">
-            {modelPrimitive}
-        </RigidBody>
-    ) : (
-        modelPrimitive
+    return (
+        <mesh position={piece.position} rotation={piece.rotation}>
+            <boxGeometry
+                args={[
+                    ROAD_GEOMETRY.width,
+                    ROAD_GEOMETRY.height,
+                    ROAD_GEOMETRY.length,
+                ]}
+            />
+            <meshStandardMaterial color="#444444" />
+        </mesh>
     )
 }
 
