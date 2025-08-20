eb41a68a9a92b53d12af1b47aaa22ec4b0d80318
Benjamín Isasmendi C. <bisasmendic@gmail.com>
2025-08-20 01:00:52 -0400

    admin track-editor now using new system track & delete old files of old system track

--- src/lib/components/track-editor/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as TrackEditorCanvas } from './TrackEditorCanvas'
-export { default as TrackEditorScene } from './TrackEditorScene'
-export { default as TrackEditorControlPanel } from './TrackEditorControlPanel'
-export { default as TrackEditorProviders } from './TrackEditorProviders'
